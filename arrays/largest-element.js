8188ad1fa95b4e6511d7985e4abbd5f923b174cd
VAMSI <vamsikrishnareddybevara@gmail.com>
2020-07-20 11:14:06 +0530

    Algorithm to find index of larget element in an array in O(n) time complexity.

---
+++ arrays/largest-element.js
@@ -0,0 +1,11 @@
+function findLargestElement(arr) {
+  // O(n) - T , O(1) - S
+  let result = 0;
+  for(let i = 1; i < arr.length; i++){
+    if(arr[i] > arr[result]) {
+      result  = i
+      }
+  }
+  return result;
+}
+
